edebcd0f42bb27682d60e5e1a1988010ebc2df96
Leqi Lu <pototoland@gmail.com>
2017-11-19 14:43:56 -0800

    created website layout

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ var fs = require('fs'); //include file system module
 var port = process.env.PORT || 8080;
 
 // serve static files
-app.use('/css', express.static(path.join(__dirname + '/css')));
+app.use('/public', express.static('public'));
 
-// create server
-http.createServer(function(req, res){
-  fs.readFile('index.html', function(err, data){
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(data);
-    res.end();
-  });
-}).listen(port);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port);
